61d5ebafebda7335fc0b0c7afc0db32228e3bd52
David Botton <david.botton@mail.com>
2024-02-27 14:30:46 +0100

    Première vue en utilisant ViewSet (faciliter)

--- fExtra/core/views.py
+++ fExtra/core/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Profile, Enfant, FraisExtraordinaire, IndiceSante, Correspondance
+from .serializers import (
+    ProfileSerializer,
+    EnfantSerializer,
+    FraisExtraordinaireSerializer,
+    IndiceSanteSerializer,
+    CorrespondanceSerializer,
+)
 
-# Create your views here.
+# TODO: permissions, filtrage pagination tri, testing #
+
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+
+class EnfantViewSet(viewsets.ModelViewSet):
+    queryset = Enfant.objects.all()
+    serializer_class = EnfantSerializer
+
+
+class FraisExtraordinaireViewSet(viewsets.ModelViewSet):
+    queryset = FraisExtraordinaire.objects.all()
+    serializer_class = FraisExtraordinaireSerializer
+
+
+class IndiceSanteViewSet(viewsets.ModelViewSet):
+    queryset = IndiceSante.objects.all()
+    serializer_class = IndiceSanteSerializer
+
+
+class CorrespondanceViewSet(viewsets.ModelViewSet):
+    queryset = Correspondance.objects.all()
+    serializer_class = CorrespondanceSerializer
