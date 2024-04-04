a285c8f5b1bf6333141610ad740880b5ff913625
Arnaud Mahieu <94074921+Serenity1909@users.noreply.github.com>
2024-04-04 22:13:08 +0200

    test connexion render

--- fExtra/settings.py
+++ fExtra/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'fExtra.accounts.apps.MainConfig',
+    'fExtra.accounts.apps.MainConfig',
     'crispy_forms',
     'crispy_bootstrap5',
 ]
@@ -76,7 +76,7 @@ LOCALE_PATHS = (
     os.path.join(BASE_DIR, 'locale/'),
 )
 
-ROOT_URLCONF = 'fExtra.fExtra.urls'
+ROOT_URLCONF = 'fExtra.urls'
 
 TEMPLATES = [
     {
@@ -89,13 +89,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                # 'fExtra.accounts.context.user_role',
+                'fExtra.accounts.context.user_role',
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'fExtra.fExtra.wsgi.application'
+WSGI_APPLICATION = 'fExtra.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
