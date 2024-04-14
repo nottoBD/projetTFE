5493f51b8317ee9cde850f99cb078e20721fd976
David Botton <david.botton@mail.com>
2024-04-14 11:29:29 +0200

    Ajout : serving Heroku staticfiles

--- fExtra/settings.py
+++ fExtra/settings.py
@@ -188,5 +188,5 @@ LOGOUT_REDIRECT_URL = '/accounts/login'
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'media')
 ANONYMOUS_USER_NAME = None
