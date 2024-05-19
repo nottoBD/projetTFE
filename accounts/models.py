561b052e00f085c8840dc25164a9ecdecbb93167
David Botton <david.botton@mail.com>
2024-05-19 12:06:55 +0200

    Fix AbstractUser, username

--- accounts/models.py
+++ accounts/models.py
@@ -1,16 +1,15 @@
 from django.contrib.auth.models import BaseUserManager, PermissionsMixin, AbstractUser
 from django.db import models
 from django.utils import timezone
-from django.utils.translation import gettext_lazy as _
 from guardian.shortcuts import assign_perm
 
 from .validations import validate_image
 
 
-class MyUserManager(BaseUserManager):
+class CustomUserManager(BaseUserManager):
     def create_user(self, email, role="parent", password=None, **extra_fields):
         if not email:
-            raise ValueError(_('Users must have an email address'))
+            raise ValueError('Users must have an email address')
 
         email = self.normalize_email(email)
         user = self.model(email=email, role=role, **extra_fields)
@@ -34,13 +33,14 @@ class User(AbstractUser, PermissionsMixin):
         ('magistrate', 'Magistrate'),
         ('parent', 'Parent'),
     ]
-    email = models.EmailField(verbose_name=_('e-mail address'), unique=True, max_length=255)
+    username = models.CharField(max_length=150, unique=False, null=True, blank=True)
+    email = models.EmailField(verbose_name='e-mail address', unique=True, max_length=255)
     is_active = models.BooleanField(default=True)
     role = models.CharField(max_length=13, choices=ROLE_CHOICES, default='parent')
     is_staff = models.BooleanField(default=False)
     gender = models.CharField(max_length=1, choices=[('M', 'Male'), ('F', 'Female'), ('X', 'They')], null=True, blank=True, default=' ')
-    last_name = models.CharField(_('last name'), max_length=150, blank=True)
-    first_name = models.CharField(_('first name'), max_length=30, blank=True)
+    last_name = models.CharField('last name', max_length=150, blank=True)
+    first_name = models.CharField('first name', max_length=30, blank=True)
     date_of_birth = models.DateField(default=timezone.now)
     telephone = models.CharField(max_length=16, null=True, blank=True)
     address = models.CharField(null=True, blank=True, max_length=75)
@@ -49,30 +49,31 @@ class User(AbstractUser, PermissionsMixin):
     profile_image = models.ImageField(upload_to='profile_images/', default='profile_images/default.jpg', validators=[validate_image])
 
     USERNAME_FIELD = 'email'
-    objects = MyUserManager()
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+    objects = CustomUserManager()
 
     def __str__(self):
         return f"{self.last_name} {self.first_name} ({self.email})"
 
     @property
     def is_administrator(self):
-        return self.role == _('administrator')
+        return self.role == 'administrator'
 
     @property
     def is_magistrate(self):
-        return self.role == _('magistrate')
+        return self.role == 'magistrate'
 
     @property
     def is_parent(self):
-        return self.role == _('parent')
+        return self.role == 'parent'
 
 
 class MagistrateParent(models.Model):
-    magistrate = models.ForeignKey(User, related_name=_('parents_assigned'), on_delete=models.CASCADE)
-    parent = models.ForeignKey(User, related_name=_('magistrates_assigned'), on_delete=models.CASCADE)
+    magistrate = models.ForeignKey(User, related_name='parents_assigned', on_delete=models.CASCADE)
+    parent = models.ForeignKey(User, related_name='magistrates_assigned', on_delete=models.CASCADE)
 
     class Meta:
         unique_together = (('magistrate', 'parent'),)
 
     def __str__(self):
-        return _(f"{self.magistrate.email} assigned to {self.parent.email}")
+        return f"{self.magistrate.email} assigned to {self.parent.email}"
