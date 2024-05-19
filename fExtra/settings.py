49c9dc1851f8b5388ec2e6b29a708d2fa91d755f
Arnaud Mahieu <94074921+Serenity1909@users.noreply.github.com>
2024-05-19 11:02:42 +0200

    test connexion render

--- fExtra/settings.py
+++ fExtra/settings.py
@@ -122,7 +122,7 @@ WSGI_APPLICATION = 'fExtra.wsgi.application'
 #     }
 # }
 
-DATABASES = {'default': dj_database_url.parse('DATABASE_URL')}
+DATABASES = {'default': dj_database_url.parse(env('DATABASE_URL'))}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
