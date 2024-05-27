069d7912efa7ed739317a4413e7dde0004191027
Arnaud Mahieu <94074921+Serenity1909@users.noreply.github.com>
2024-05-27 19:22:43 +0200

    add payments magistrat

--- Payments/models.py
+++ Payments/models.py
@@ -29,4 +29,4 @@ class Folder(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"Folder managed by {self.magistrate} with parents {self.parent1} and {self.parent2}"
+        return f"Folder managed by {self.magistrate} with parents {self.parent1} and {self.parent2}"l
